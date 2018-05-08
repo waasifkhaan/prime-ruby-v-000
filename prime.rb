5e10b8d29a321a2e719f207e8ad19594d930e142
Wasif Khan <waasif.khaan@gmail.com>
2018-05-08 04:20:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,10 @@
 def prime?(int)
   if int < 2
     return false
+  elsif int == 2
+    return true
   elsif
-    (2..int).to_a.each do |num|  [2,3,4]
+    (2..int).to_a.each do |num|  [2,3]
       if int % num == 0
         return false
       end
